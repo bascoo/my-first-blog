f3be8af08e856055dc06ce7da128100fd652470a
bascoo <b.oosterom@live.nl>
2016-03-03 15:45:34 +0100

    stories

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', 'home.views.index'),
+    url(r'^verhalen/$', 'home.views.verhalen'),
 	url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace="blog")),
