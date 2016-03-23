83052638a66824f04a94719105db6a16b476e471
bascoo <b.oosterom@live.nl>
2016-03-23 11:30:27 +0100

    latests update, work on polls button

--- iamgrey/views.py
+++ iamgrey/views.py
@@ -11,7 +11,6 @@ class IndexView(generic.ListView):
     latest_story = Story.objects.latest('date_published')
 
     def get_queryset(self):
-        """Return the last five published questions."""
         return Story.objects.filter(date_published__lte=timezone.now()).order_by('-date_published')
 
     def get_context_data(self, *args, **kwargs):
